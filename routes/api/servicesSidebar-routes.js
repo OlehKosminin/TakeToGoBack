2fd85500dc947d7c9394f165425911df11a5cffa
Olehys <arzamazova13@gmail.com>
2023-06-29 14:04:55 +0300

    init repository with base settings

--- routes/api/servicesSidebar-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const ctrl = require("../../controllers/servicesSidebar-controllers");
-
-const router = express.Router();
-
-router.get("/", ctrl.getPartnerInfo);
-
-module.exports = router;
